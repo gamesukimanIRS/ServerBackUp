06edadc202944c3ff18f8a03bbf27d04875aa0b1
gamesukimanIRS <gamesukiman@gmail.com>
2018-01-08 01:21:30 +0900

    Update Main.php

--- src/ServerBackUp/Main.php
+++ src/ServerBackUp/Main.php
@@ -99,13 +99,3 @@ class Main extends PluginBase implements Listener{
 		}
 	}
 }
-
-class BackUpTask extends PluginTask{
-	public function __construct(PluginBase $owner) {
-		$this->owner = $owner;
-	}
-
-	public function onRun(int $currentTick){
-		$this->owner->serverBackUp();
-	}
-}
